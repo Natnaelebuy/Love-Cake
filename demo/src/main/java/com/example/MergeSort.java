5ed4bbe5d529e62c802fb283d5cf64f6bad8b12f
Natnael <natnaelebuy@gmail.com>
2022-03-24 21:28:02 -0700

    change some summary

--- demo/src/main/java/com/example/MergeSort.java
+++ demo/src/main/java/com/example/MergeSort.java
@@ -83,18 +83,18 @@ public class MergeSort {
     // split the array in half, sort each half recursively, then merge
     int middle = left + ((right - left) / 2);
 
-    //int leftStart = left;
-    //int leftEnd = middle;
-    //int rightStart = middle + 1;
-    //int rightEnd = right;
+    int leftStart = left;
+    int leftEnd = middle;
+    int rightStart = middle + 1;
+    int rightEnd = right;
 
-    //System.out.println("merge sort left");
-    //print(arr, leftStart, leftEnd);
-    //sort(arr, leftStart, leftEnd);
+    System.out.println("merge sort left");
+   print(arr, leftStart, leftEnd);
+   sort(arr, leftStart, leftEnd);
 
-    //System.out.println("merge sort right");
-    //print(arr, rightStart, rightEnd);
-    //sort(arr, rightStart, rightEnd);
+    System.out.println("merge sort right");
+   print(arr, rightStart, rightEnd);
+   sort(arr, rightStart, rightEnd);
 
     // merge the two sorted arrays
     merge(arr, left, middle, right);
@@ -152,3 +152,47 @@ public class MergeSort {
     count += right - left + 1;
   }
 }
+
+// Java program for implementation of Insertion Sort
+class InsertionSort {
+	/*Function to sort array using insertion sort*/
+	void sort(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 1; i < n; ++i) {
+			int key = arr[i];
+			int j = i - 1;
+
+			/* Move elements of arr[0..i-1], that are
+			greater than key, to one position ahead
+			of their current position */
+			while (j >= 0 && arr[j] > key) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[j + 1] = key;
+		}
+	}
+
+	/* A utility function to print array of size n*/
+	static void printArray(int arr[])
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; ++i)
+			System.out.print(arr[i] + " ");
+
+		System.out.println();
+	}
+
+	// Driver method
+	public static void main(String args[])
+	{
+		int arr[] = { 12, 11, 13, 5, 6 };
+
+		InsertionSort ob = new InsertionSort();
+		ob.sort(arr);
+
+		printArray(arr);
+	}
+} /* This code is contributed by Rajat Mishra. */
+
